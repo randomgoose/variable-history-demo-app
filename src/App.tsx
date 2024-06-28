a70e6055935fd77be92822fdfc7afeeb974c6969
Chen Chen <c.chen.ed5557@gmail.com>
2024-06-29 06:08:58 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,30 @@ function App() {
 
   return (
     <div data-theme='Default Light'>
-      <div style={{ background: 'var(--✦-bg-default-default)', width: 390, height: 844 }}>
+      <div style={{ background: 'var(---bg-default-default)', width: 390, height: 844 }}>
         <div style={{ padding: 8 }}>
           <div style={{
             textAlign: 'left',
             borderRadius: 24,
             height: 344,
             padding: 24,
-            background: 'var(--✦-bg-brand-tertiary)',
+            background: 'var(---bg-brand-tertiary)',
             position: 'relative'
           }}>
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
               <div>
-                <div style={{ color: 'var(--✦-text-default-secondary)' }}>Balance</div>
-                <div style={{ color: 'var(--✦-text-default-default)', fontSize: 28, fontWeight: 'bold' }}>
+                <div style={{ color: 'var(---text-default-secondary)' }}>Balance</div>
+                <div style={{ color: 'var(---text-default-default)', fontSize: 28, fontWeight: 'bold' }}>
                   60,000.
                   <span style={{ fontSize: 16 }}>00</span>
                 </div>
-                <div style={{ color: 'var(--✦-text-success-default)', fontWeight: 'bold' }}>+$4,000 (7.14%)</div>
+                <div style={{ color: 'var(---text-success-default)', fontWeight: 'bold' }}>+$4,000 (7.14%)</div>
               </div>
 
               <button style={{
-                background: 'var(--✦-bg-default-default)',
-                border: '1px solid var(--✦-border-default-default)',
-                color: 'var(--✦-text-default-default)',
+                background: 'var(---bg-default-default)',
+                border: '1px solid var(---border-default-default)',
+                color: 'var(---text-default-default)',
               }}>
                 Deposit
               </button>
